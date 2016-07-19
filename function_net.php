2728296b05794b0544f301f3ef1ac5fb8886d287
leo <leo19920823@gmail.com>
2016-07-20 01:33:54 +0800

    修改命名空间

---
+++ function_net.php
@@ -0,0 +1,37 @@
+<?php
+namespace leo;
+/*
+ *curl请求
+ *
+ */
+function curl_post($url,$fields)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.648; .NET CLR 3.5.21022)");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,$fields);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_COOKIE, 'PHPSESSID=' . C('CURL_SESSION_ID'));
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+/*
+ * curl请求
+ *
+ */
+function curl_get($url)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.648; .NET CLR 3.5.21022)");
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
