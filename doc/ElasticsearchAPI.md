149c4a21669489a69e48dc76655d74e2c3466532
leo <leo@zhugefang.com>
2016-12-18 00:56:59 +0800

    增加elasticsearchAPI

---
+++ doc/ElasticsearchAPI.md
@@ -0,0 +1,89 @@
+[:TOC]
+
+# ElasticsearchAPI 概述
+
+## 简介
+本类采用es的官方REST API 
+
+## 方法原型
+```php
+// 使用地址构造
+ElasticsearchAPI __construct($host)
+
+// 尝试是否畅通
+bool ping()
+
+// 插入一条数据,指定id
+bool function set($index, $type, $id, $data)
+
+// 获取指定一条数据
+array get($index, $type, $id)
+
+// 统计某个type的文档数
+int total($index, $type)
+
+// 更新字段
+bool save($index, $type, $id, $data)
+
+// 删除数据
+bool del($index, $type, $id)
+
+```
+
+# 使用
+
+## 实例化 
+1. 首先拿到es集群中某一节点的链接
+2. 在实例化的时候传入es链接
+```php
+$es = new \Leo\ElasticsearchAPI("http://127.0.0.1:9200");
+```
+
+## 设置数据
+```php 
+$index = 'test_index';
+$type = 'test_type';
+$id = 1;
+$data = [
+    "name" => "leo",
+    "age" => 18,
+    "addr" => '北京'
+];
+$es->set($index, $type, $id, $data);
+```
+
+## 获取数据
+```php 
+$index = 'test_index';
+$type = 'test_type';
+$id = 1;
+$es->get($index, $type, $id);
+```
+
+## 更新数据
+```php 
+$index = 'test_index';
+$type = 'test_type';
+$id = 1;
+$data = [
+    "addr" => '上海',
+    "email" => "leo19920823@gmail.com"
+];
+$es->save($index, $type, $id, $data);
+```
+
+## 删除数据
+```php 
+$index = 'test_index';
+$type = 'test_type';
+$id = 1;
+$es->del($index, $type, $id);
+```
+
+## 统计总数
+```php 
+$index = 'test_index';
+$type = 'test_type';
+$es->total($index, $type);
+```
+
---
+++ src/Leo/ElasticsearchAPI.php
@@ -0,0 +1,159 @@
+<?php
+namespace Leo;
+
+
+/*
+ * ElasticsearchAPI
+ * Elasticsearch的原生REST API
+ */
+class ElasticsearchAPI
+{
+    private $host = "";
+
+    // 使用地址构造
+    public function __construct($host)
+    {
+        $this->host = $host;
+    }
+
+    // 尝试是否畅通
+    public function ping()
+    {
+        $res = $this->call('GET', '/');
+        $arr = json_decode($res, true);
+
+        if (isset($arr['status']) && 200 == $arr['status']) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 插入一条数据,指定id
+    public function set($index, $type, $id, $data)
+    {
+        $uri = "/$index/$type/$id";
+        $method = 'PUT';
+
+        $res = $this->call($method, $uri, $data);
+        $arr = json_decode($res, true);
+
+        if (isset($arr['_id']) && $id == $arr['_id']) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 获取指定一条数据
+    public function get($index, $type, $id)
+    {
+        $uri = "/$index/$type/$id";
+        $method = 'GET';
+
+        $res = $this->call($method, $uri);
+        $arr = json_decode($res, true);
+
+        if (isset($arr['found']) && $arr['found']) {
+            return $arr['_source'];
+        } else {
+            return false;
+        }
+    }
+
+    // 统计某个type的文档数
+    public function total($index, $type)
+    {
+        $uri = "/$index/$type/_count";
+        $method = 'GET';
+
+        $query = [
+            'query' => [
+                'match_all' => (object)[]
+            ]
+        ];
+
+        $res = $this->call($method, $uri, $query);
+        $arr = json_decode($res, true);
+
+        if (isset($arr['count']) && $arr['count']) {
+            return $arr['count'];
+        } else {
+            return false;
+        }
+    }
+
+    // 更新字段
+    public function save($index, $type, $id, $data)
+    {
+        $uri = "/$index/$type/$id/_update";
+        $method = 'POST';
+
+        $res = $this->call($method, $uri, ['doc' => $data]);
+        $arr = json_decode($res, true);
+
+        if (isset($arr['_id']) && $id == $arr['_id']) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 删除数据
+    public function del($index, $type, $id)
+    {
+        if (!$id) {
+            return false;
+        }
+        $uri = "/$index/$type/$id";
+        $method = 'DELETE';
+
+        $res = $this->call($method, $uri);
+        $arr = json_decode($res, true);
+
+        die($res);
+
+        if (isset($arr['found']) && $arr['found']) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 设置mapping
+    public function setMapping($index, $type, $mapping)
+    {
+    }
+
+    // 获取索引
+    public function getMapping($index, $type)
+    {
+        $uri = "/$index/_mapping";
+        $method = 'GET';
+
+        return $this->call($method, $uri);
+    }
+
+    // 执行请求
+    private function call($method, $uri, $data = [])
+    {
+        $ch = curl_init($this->getHost(). $uri);
+        $data_string = json_encode($data);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string)
+        )
+        );
+        $result = curl_exec($ch);
+        return $result;
+    }
+
+    // 获取host地址
+    private function getHost()
+    {
+        return $this->host;
+    }
+}
